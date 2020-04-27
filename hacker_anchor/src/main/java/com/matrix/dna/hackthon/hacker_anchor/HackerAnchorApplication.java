9bc793d921a65b5a5e66fb5e8280139b042819bb
abhishek <tech.parsai@gmail.com>
2020-04-27 21:17:37 +0530

    setup done

---
+++ hacker_anchor/src/main/java/com/matrix/dna/hackthon/hacker_anchor/HackerAnchorApplication.java
@@ -0,0 +1,13 @@
+package com.matrix.dna.hackthon.hacker_anchor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackerAnchorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackerAnchorApplication.class, args);
+	}
+
+}
---
+++ hacker_anchor/src/test/java/com/matrix/dna/hackthon/hacker_anchor/HackerAnchorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matrix.dna.hackthon.hacker_anchor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackerAnchorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
